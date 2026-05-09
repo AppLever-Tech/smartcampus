08037254fc3d356323550b9820da9ff05cc63a72
rachana-182001 <rachana2001ak@gmail.com>
2026-05-09 16:26:24 +0530

    course page

---
+++ lib/screens/courses_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CoursesPage extends StatelessWidget {
+  const CoursesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Courses'),
+      ),
+      body: const Center(
+        child: Text(
+          'Courses Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
